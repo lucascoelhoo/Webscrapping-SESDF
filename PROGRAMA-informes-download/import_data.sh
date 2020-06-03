a7a863dcb794e1fe441b32e140e4566b5ddd9591
Lucas Coelho de Almeida <luccoelhoo@gmail.com>
2020-06-02 22:07:19 -0300

    Adicionou script que popula banco.

---
+++ PROGRAMA-informes-download/import_data.sh
@@ -0,0 +1,7 @@
+mkdir output
+
+for f in *.csv; do awk -v d="$f" -F"," 'BEGIN {OFS = ","} FNR==1{$(NF+1)="dataExtracao"} FNR>1{$(NF+1)=d;} 1' RS="\r\n" $f > output/$f; done
+
+cd output
+
+for f in *.csv; do docker cp $f MONGODB_BSB_DATA_COVID:/tmp/; docker exec MONGODB_BSB_DATA_COVID mongoimport --type csv -d bsbdatacovid -c regions --headerline --file /tmp/$f; done
